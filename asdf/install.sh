9cca3ef0f3aa3617f857a99c6641b4e14c24a6cb
Garth Braithwaite <garthdb@gmail.com>
2020-09-15 15:17:35 -0600

    added asdf

---
+++ asdf/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# asdf
+#
+# This installs the version management library asdf and some of the plugins
+
+# Check for asdf
+if test ! $(which asdf)
+then
+  echo "  Installing asdf for you."
+  brew install asdf > /tmp/asdf-install.log
+fi
+
+# Check for nodejs plugin
+if [[ "$(asdf plugin list)" != *nodejs* ]]
+then
+  echo "  Nodejs asdf plugin is being installed"
+  asdf plugin add nodejs
+fi
+
+# Check for nodejs plugin
+# if [[ "$(asdf plugin list)" != *ruby* ]]
+# then
+#   echo "  Ruby asdf plugin is being installed"
+#   asdf plugin add ruby
+# fi
+
+exit 0
